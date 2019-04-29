46903be0eec2e73ab9f49a4d5e4e7f499e66f64d
ksrawr <kenneth.surban62@gmail.com>
2019-04-28 23:39:47 -0700

    Hide API info

--- src/config/config.js
+++ src/config/config.js
@@ -2,28 +2,12 @@ import Rebase from 're-base';
 import firebase from "firebase";
 import 'firebase/database';
 
-// const DB_CONFIG = {
-//   apiKey: (process.env.REACT_APP_FB_API_KEY),
-//   authDomain: (process.env.REACT_APP_FB_AUTH_DOMAIN),
-//   databaseURL: (process.env.REACT_APP_FB_DATABASE_URL),
-//   projectId: (process.env.REACT_APP_FB_PROJECT_ID),
-//   storageBucket: (process.env.REACT_APP_FB_STORAGE_BUCKET),
-//   messagingSenderId: "488864452158"
-// };
-
-
-/**
-
-	Firebase Database Rules Updated. Database Locked for Writing to.
-
-*/
-
 const DB_CONFIG = {
- 	apiKey: "AIzaSyD0XrvHX0TAXJoHNg0G6Y8vgts33NY_waM",
-  authDomain: "practicefusion-9ae82.firebaseapp.com",
-  databaseURL: "https://practicefusion-9ae82.firebaseio.com",
-  projectId: "practicefusion-9ae82",
-  storageBucket: "practicefusion-9ae82.appspot.com",
+  apiKey: (process.env.REACT_APP_FB_API_KEY),
+  authDomain: (process.env.REACT_APP_FB_AUTH_DOMAIN),
+  databaseURL: (process.env.REACT_APP_FB_DATABASE_URL),
+  projectId: (process.env.REACT_APP_FB_PROJECT_ID),
+  storageBucket: (process.env.REACT_APP_FB_STORAGE_BUCKET),
   messagingSenderId: "488864452158"
 };
 
